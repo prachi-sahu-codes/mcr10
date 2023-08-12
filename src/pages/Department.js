55b2c7bef1f9aecd67ff0bbc4d2e6849fcfd6613
SahuPrachi <prachisahu998@gmail.com>
2023-08-12 18:47:33 +0530

    feat: routing done

--- src/pages/Department.js
+++ src/pages/Department.js
@@ -5,13 +5,22 @@ export const Department = () => {
   const navigate = useNavigate();
   return (
     <div className="flex gap-10 h-fit m-10">
-      <div className=" bg-slate-100 p-6 px-9 rounded-md shadow-md cursor-pointer active:shadow-sm">
+      <div
+        className=" bg-slate-100 hover:bg-slate-200 p-6 px-9 rounded-md shadow-md cursor-pointer active:shadow-sm"
+        onClick={() => navigate("/products")}
+      >
         <h1 className="text-xl font-bold text-center">Kitchen</h1>
       </div>
-      <div className=" bg-slate-100 p-6 px-9 rounded-md shadow-md cursor-pointer active:shadow-sm">
+      <div
+        className=" bg-slate-100 hover:bg-slate-200 p-6 px-9 rounded-md shadow-md cursor-pointer active:shadow-sm"
+        onClick={() => navigate("/products")}
+      >
         <h1 className="text-xl font-bold text-center">Clothing</h1>
       </div>
-      <div className=" bg-slate-100 p-6 px-9 rounded-md shadow-md cursor-pointer active:shadow-sm">
+      <div
+        className=" bg-slate-100 hover:bg-slate-200 p-6 px-9 rounded-md shadow-md cursor-pointer active:shadow-sm"
+        onClick={() => navigate("/products")}
+      >
         <h1 className="text-xl font-bold text-center">Toys</h1>
       </div>
     </div>
